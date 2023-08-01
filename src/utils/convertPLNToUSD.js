bc7bafc977af5d765a9279ffa316351cdbfecd73
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-08-01 11:15:47 +0200

    Add _test_ folder in Utils

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,27 @@
 export const convertPLNToUSD = (PLN) => {
 
-  const PLNtoUSD = PLN / 3.5;
-  
+  const PLNtoUSD = parseFloat(PLN) / 3.5;
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
   });
+
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  }
+
+  if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
+  if (PLN === null) {
+    return NaN;
+  }
 
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 }
